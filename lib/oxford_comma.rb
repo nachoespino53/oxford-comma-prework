44cb6d5d8a3eb43a0d4184b869b2ef4f63527145
Ignacio R Espino <irespino@capozios.com>
2018-08-28 20:30:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array[0]
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    return "#{array.join(", ")}, and #{last_item}"
+  end
 end
